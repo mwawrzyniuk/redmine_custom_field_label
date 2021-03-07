b520d5e195fc92f812fe219b3438ce8c54d4b3b4
Michal_W <m.wawrzyniuk@gmail.com>
2021-03-07 15:01:05 +0000

    Updated author details

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 Redmine::Plugin.register :redmine_custom_field_label do
-  name 'Redmine Custom Field Label plugin'
-  author 'Askarov Alikhan'
+  name 'Redmine custom field to create wide description'
+  author 'Michal Wawrzyniuk'
   description 'This is a plugin for Redmine which adds Label type to Custom Fields'
-  version '0.9.0'
-  url 'https://github.com/Legomegger/custom_field_label'
-  author_url 'https://github.com/Legomegger'
+  version '0.1.0'
+  url 'https://github.com/mwawrzyniuk/redmine_custom_field_label'
+  author_url 'https://github.com/mwawrzyniuk'
 
   # include patch to kernel
   require_dependency 'redmine_custom_field_label/patches/label_format_patch'
